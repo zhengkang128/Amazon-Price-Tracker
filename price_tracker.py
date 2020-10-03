fbd7a8abdff030ad1f9b3f278adf7fbfa912461b
zhengkang128 <72126038+zhengkang128@users.noreply.github.com>
2020-10-03 17:14:11 +0800

    Add files via upload

--- price_tracker.py
+++ price_tracker.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import FormatStrFormatter
 from statistics import mean 
-
+import numpy as np
 
 
 def scrap(URL, target, email):
@@ -50,113 +50,122 @@ def scrap(URL, target, email):
     if shipping_price is None:
         shipping_price = "0"
     else:
-        shipping_price = shipping_price.get_text()    
+        shipping_price = shipping_price.get_text()
+    if ("Shipping" not in shipping_price) and ("Import" not in shipping_price):
+        shipping_price = "0"
     #float conversion
     converted_price = ""
     converted_shipping = ""
     for i in price:
         if (i.isdigit() or i=="."):
             converted_price = converted_price + i
+    
     for i in shipping_price:
         if (i.isdigit() or i=="."):
-            converted_shipping = converted_shipping + i
+            converted_shipping = converted_shipping + i            
             
-            
-    converted_price = float(converted_price)
-    if (converted_shipping == ""):
-        converted_shipping = 0
-    converted_shipping = float(converted_shipping)
-    total_price = converted_price + converted_shipping
+    converted_price = round(float(converted_price),2)
+    converted_shipping = round(float(converted_shipping),2)
+    total_price = round(converted_price + converted_shipping,2)
 
     #To Check Successful Scrape
     print(title)
-    print(converted_price)
-    print(converted_shipping)
-    print(total_price)
-
-    #Send mail
-    if (converted_price+converted_shipping <target):
-        send_mail(title, converted_price, converted_shipping, email, URL)
-        
-    #mycursor.execute("SELECT * FROM price_information WHERE product_name = " + str(title) + " ORDER BY id DESC LIMIT 1;")
-    mycursor.execute("SELECT * FROM price_information WHERE product_name = " + "\"" +  str(title) + "\"" + " ORDER BY id DESC LIMIT 1;")  
-    myresult = mycursor.fetchall()
-
-  
-    query = "INSERT INTO price_information (product_name, date_retrieved, total_price, product_price, shipping_price) VALUES (%s, %s, %s, %s, %s);"
+    print("Product Price: " + str(converted_price))
+    print("Shipping Price: " + str(converted_shipping))
+    print("Total Price: " + str(total_price))
     now = datetime.now()
-    d1 = now.strftime('%Y-%m-%d %H:%M:%S')
-    arg = (title, d1, total_price, converted_price, converted_shipping)
-    mycursor.execute(query,arg)
-    connection.commit()
-    print("Updated Database")
-        
-    if (len(myresult)!=0):
-        if (abs(myresult[0][3]-total_price)>=0.01):
-            send_mail_drop_price(title, converted_price, converted_shipping, email,URL)
-            print("Change in price")
-        else:
-            print("No change in price")
-    else:
-        print("New item")
+    d1 = now.strftime('%Y-%m-%d %H:%M:%S')    
+    #Send mail
+    if total_price!= 0:
+        if (converted_price+converted_shipping <target):
+            send_mail(title, converted_price, converted_shipping, email, URL)
+            
+        mycursor.execute("SELECT * FROM price_information WHERE product_name = " + "\"" +  str(title) + "\"" + " ORDER BY id DESC LIMIT 1;")  
+        myresult = mycursor.fetchall()
 
-    query = "SELECT id FROM products WHERE product_name = " + "\"" +  str(title) + "\""
-    mycursor.execute(query)
-    result2 = mycursor.fetchall()
-
-    if len(result2)==0:
-        query = "INSERT INTO products (product_name, comments) VALUES (%s, %s);"
-        arg = (title, URL)  
+      
+        query = "INSERT INTO price_information (product_name, date_retrieved, total_price, product_price, shipping_price) VALUES (%s, %s, %s, %s, %s);"
+        now = datetime.now()
+        d1 = now.strftime('%Y-%m-%d %H:%M:%S')
+        arg = (title, d1, total_price, converted_price, converted_shipping)
         mycursor.execute(query,arg)
         connection.commit()
-        print("Updated Products Table")
+        print("Updated Database")
+            
+        if (len(myresult)!=0):
+            if (abs(myresult[0][3]-total_price)>=0.01):
+                send_mail_drop_price(title, converted_price, converted_shipping, email,URL)
+                print("Change in price")
+            else:
+                print("No change in price")
+        else:
+            print("New item")
+        print("")
 
-    #Product graph
-    query = "SELECT * FROM price_information pi JOIN products p ON p.product_name = pi.product_name WHERE pi.product_name = " + "\"" + title + "\" ORDER BY pi.date_retrieved"
-    mycursor.execute(query)
-    myresult = mycursor.fetchall()
+        query = "SELECT id FROM products WHERE product_name = " + "\"" +  str(title) + "\""
+        mycursor.execute(query)
+        result2 = mycursor.fetchall()
 
-    product_id = myresult[0][6]
-    product_name = myresult[0][1]
-    total_price = []
-    product_price = []
-    shipping_price = []
-    date = []
+        if len(result2)==0:
+            query = "INSERT INTO products (product_name, comments) VALUES (%s, %s);"
+            arg = (title, URL)  
+            mycursor.execute(query,arg)
+            connection.commit()
+            print("Updated Products Table")
 
-    for rows in myresult:
-        total_price.append(rows[3])
-        product_price.append(rows[4])
-        shipping_price.append(rows[5])
-        date.append(rows[2])
-    fig, ax = plt.subplots(figsize=(20, 15))
+        #Product graph
+        query = "SELECT * FROM price_information pi JOIN products p ON p.product_name = pi.product_name WHERE pi.product_name = " + "\"" + title + "\" ORDER BY pi.date_retrieved"
+        mycursor.execute(query)
+        myresult = mycursor.fetchall()
 
-    mean_total = round(mean(total_price),2)
-    mean_product = round(mean(product_price),2)
-    mean_shipping = round(mean(shipping_price),2)
+        product_id = myresult[0][6]
+        product_name = myresult[0][1]
+        total_price = []
+        product_price = []
+        shipping_price = []
+        date = [] 
 
-    ax.plot(date,
-            total_price,
-            color='purple', label = "Total Price (mean = $" + str(mean_total) + ")")
+        for rows in myresult:
+            total_price.append(rows[3])
+            product_price.append(rows[4])
+            shipping_price.append(rows[5])
+            date.append(rows[2])
+        fig, ax = plt.subplots(figsize=(20, 15))
 
-    ax.plot(date,
-            product_price,
-            color='blue', label = "Product Price (mean = $" + str(mean_product) + ")")
+        mean_total = round(mean(total_price),2)
+        mean_product = round(mean(product_price),2)
+        mean_shipping = round(mean(shipping_price),2)
 
-    ax.plot(date,
-            shipping_price,
-            color='red', label = "Shipping Price (mean = $" + str(mean_shipping) + ")")
+        ax.plot(date,
+                total_price,
+                color='purple', label = "Total Price (mean = $" + str(mean_total) + ")")
 
-    ax.set(xlabel="Date and Time",
-           ylabel="Price (SGD)",
-           title="Changes of Pricing for \n" + product_name + "\n")
-    ax.legend()
+        ax.plot(date,
+                product_price,
+                color='blue', label = "Product Price (mean = $" + str(mean_product) + ")")
 
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
-    fig.autofmt_xdate()
+        ax.plot(date,
+                shipping_price,
+                color='red', label = "Shipping Price (mean = $" + str(mean_shipping) + ")")
 
-    plt.savefig("results/product_num" + str(product_id) +'.png')
+        ax.set(xlabel="Date and Time",
+               ylabel="Price (SGD)",
+               title="Changes of Pricing for \n" + product_name + "\n")
+        ax.legend()
+        upper = 5 * round(max(total_price)/5)
 
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+        fig.autofmt_xdate()
+        ax.grid(which='minor', alpha=0.8)
+        plt.grid()
+
+
+        plt.savefig("results/product_num" + str(product_id) +'.png')
+        plt.close()
+
+    return [title.split(",")[0], converted_price, converted_shipping, d1]
+    
 
 
 
@@ -218,7 +227,19 @@ def send_mail_drop_price(title, price, shipping, email, URL):
     print("Mail sent")
     server.quit()
 
-
+def autolabel(rects1,rects2):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect1,rect2 in zip(rects1,rects2):
+        height = round(rect1.get_height() + rect2.get_height(),2)
+        if height == 0:
+            label = "Item Unavailable"
+        else:
+            label = height
+        ax.annotate('{}'.format(label),
+                    xy=(rect1.get_x() + rect1.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
     
 
 # Using readlines() 
@@ -239,6 +260,7 @@ for line in Lines:
     count = count + 1
 
 numLinks = count//2
+print(numLinks)
         
     
 print("Notification will be sent to " + email)
@@ -250,17 +272,95 @@ print("")
 
 start = int(round(time.time()))
 
-    #Execute one time
+#Execute one time
+print("")
+print("Fetching results")
+pricing_results = []
+product_current = []
+current_price = []
+current_shipping = []
 for i in range(numLinks):
-    scrap(URL_list[i],price_target[i], email)
-    start = int(round(time.time()))
+    pricing_results = scrap(URL_list[i],price_target[i], email)
+
+    product_current.append(pricing_results[0][0:10])
+    current_price.append(pricing_results[1])
+    current_shipping.append(pricing_results[2])
+    timing = (pricing_results[3])
+            
+indexes = [z for z in range(numLinks)]
+width = 0.15
+
+fig, ax = plt.subplots(figsize=(20, 15))
+
+rects1=ax.bar(indexes, current_price, width, color='b', align='center')
+rects2=ax.bar(indexes, current_shipping, width, bottom=current_price, color='r', align='center')
+
+ax.set_ylabel('Prices in SGD')
+ax.set_title('Prices of Products retrieved at ' + timing)
+plt.xticks(indexes, product_current, rotation = 90)
+
+
+ax.legend(labels=['Product Price', 'Shipping Price'])
+ax.grid(which='minor', alpha=0.8)
+
+autolabel(rects1,rects2)
+
+
+plt.grid()
+
+plt.savefig("price_compare/price_compare" + "_" + timing.replace(" ","_").replace(":","_") + ".png")
+plt.close()
+start = int(round(time.time()))
+
     
 while (True):
-        
-    if (int(round(time.time())) - start >= (60*60)):
+
+    pricing_results = []
+    product_current = []
+    current_price = []
+    current_shipping = []
+    if (int(round(time.time())) - start >= (60*60*2)):
+        print("")
+        print("Fetching results")
         for i in range(numLinks):
-            scrap(URL_list[i],price_target[i], email)
+            pricing_results = scrap(URL_list[i],price_target[i], email)
+            
+            product_current.append(pricing_results[0][0:10])
+            current_price.append(pricing_results[1])
+            current_shipping.append(pricing_results[2])
+            timing = (pricing_results[3])
+            
+        indexes = [z for z in range(numLinks)]
+
+
+        width = 0.15
+
+        fig, ax = plt.subplots(figsize=(20, 15))
+
+        ax.bar(indexes, current_price, width, color='b', align='center')
+        ax.bar(indexes, current_shipping, width,bottom=current_price, color='r', align='center')
+
+        ax.set_ylabel('Prices in SGD')
+        ax.set_title('Prices of Products retrieved at ' + timing)
+        plt.xticks(indexes, product_current, rotation = 90)
+        upper = 5*round((max(current_price) + max(current_shipping))/5)
+
+
+        ax.legend(labels=['Product Price', 'Shipping Price'])
+        ax.grid(which='minor', alpha=0.8)
+        autolabel(rects1,rects2)
+
+        plt.grid()
+
+        plt.savefig("price_compare/price_compare" + "_" + timing.replace(" ","_").replace(":","_") + ".png")
+        plt.close()
+
+
+
+
+        
         start = int(round(time.time()))
+        
         
     
 
